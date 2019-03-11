7a6bf4a9a4283abe3847ec8e497e3e118e3aac1c
BUPTCZQ <czq@qq.com>
2019-03-11 12:58:41 +0800

    add bootstrap-mini.sh

---
+++ bootstrap-mini.sh
@@ -0,0 +1,111 @@
+#!/usr/bin/env bash
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+############################  SETUP PARAMETERS
+app_name='czq-vim-mini'
+debug_mode='0'
+fork_maintainer='0'
+
+############################  BASIC SETUP TOOLS
+msg() {
+    printf '%b\n' "$1" >&2
+}
+
+success() {
+    if [ "$ret" -eq '0' ]; then
+        msg "\33[32m[✔]\33[0m ${1}${2}"
+    fi
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+debug() {
+    if [ "$debug_mode" -eq '1' ] && [ "$ret" -gt '1' ]; then
+        msg "An error occurred in function \"${FUNCNAME[$i+1]}\" on line ${BASH_LINENO[$i+1]}, we're sorry for that."
+    fi
+}
+
+program_exists() {
+    local ret='0'
+    command -v $1 >/dev/null 2>&1 || { local ret='1'; }
+
+    # fail on non-zero return value
+    if [ "$ret" -ne 0 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+program_must_exist() {
+    program_exists $1
+
+    # throw error on non-zero return value
+    if [ "$?" -ne 0 ]; then
+        error "You must have '$1' installed to continue."
+    fi
+}
+
+variable_set() {
+    if [ -z "$1" ]; then
+        error "You must have your HOME environmental variable set to continue."
+    fi
+}
+
+lnif() {
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+    ret="$?"
+    debug
+}
+
+############################ SETUP FUNCTIONS
+
+setup_vimrc() {
+    curl -fLo ~/.vimrc \
+        https://github.com/buptczq/vimdotfile/raw/master/.vimrc.mini
+
+    ret="$?"
+    success "Downloaded vimrc-mini"
+    debug
+}
+
+setup_plug() {
+    local system_shell="$SHELL"
+    export SHELL='/bin/sh'
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    vim \
+        "+PlugInstall" \
+        "+qall"
+
+    export SHELL="$system_shell"
+
+    success "Now updating/installing plugins using Plug"
+    debug
+}
+
+############################ MAIN()
+variable_set "$HOME"
+program_must_exist "vim"
+
+setup_vimrc
+setup_plug
+
+msg             "\nThanks for installing $app_name."
